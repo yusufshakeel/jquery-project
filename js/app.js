8fad706464cec88cac8e761fa6c34c684dbe54e1
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-10-10 18:09:58 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-	$("body").prepend("<p>Hello World</p>");
+// var $;
+// var jQuery;
+// console.log($, typeof $);
+// console.log(jQuery, typeof jQuery);
+
+// $("body").append("<h1>Hello World</h1>");
+// $("body").append("<h1>Hello Yusuf</h1>");
+
+// $(document).ready(function(){
+// 	$("body").append("<h1>Hello World</h1>");
+// });
+
+$(function(){
+	$("body").append("<h1>Hello World!!!</h1>");
 });
