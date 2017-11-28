3a8665c049b8a30d404ba32ccda94e0ebbc00f26
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-11-28 11:04:10 +0530

    update

---
+++ js/add-element-outside.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	// before() method
+	// $("#sample-div1").before("<p>Para 4</p>");
+
+	// insertBefore() method
+	// $("<p>Para 4</p>").insertBefore("#sample-div1");
+	
+	// after() method
+	// $("#sample-div1").after("<p>Para 4</p>");
+
+	// insertAfter() method
+	$("<p>Para 4</p>").insertAfter("#sample-div1");
+});
