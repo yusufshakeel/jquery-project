67e3b88e7d0d64e2b9dee1338f67b0196e268da7
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-12-05 12:25:05 +0530

    update

---
+++ js/wrap.js
@@ -0,0 +1,32 @@
+$(function(){
+
+	// wrap() method
+	// $("#sample-p1").wrap("<div></div>");
+	
+	// $("#sample-p1").wrap( $("<div />", {
+	// 	"id": "sample-div1",
+	// 	"class": "sample-div1-class"
+	// }) );
+	
+	// wrapAll() method
+	// $(".sample-p").wrapAll("<div></div>");
+	
+	// $(".sample-p").wrapAll( $("<div />", {
+	// 	"id": "sample-div2",
+	// 	"class": "sample-div2-class"
+	// }) );
+	
+	// wrapInner() method
+	// $(".sample-p").wrapInner("<span></span>");
+
+	// $(".sample-p").wrapInner( $("<span />", {
+	// 	"class": "sample-span"
+	// }) );
+
+	// empty() method
+	// $("#sample-p1").empty();
+
+	// remove() method
+	$("#sample-p1").remove();
+	
+});
